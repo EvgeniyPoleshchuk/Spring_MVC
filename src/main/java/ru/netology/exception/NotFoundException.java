0a9b774eeb135077f8629f07260b7a6993f8cbd3
Evgeniy Poleshchuk <2x2great@gmail.com>
2023-08-21 14:28:18 +0300

    Initial commit

---
+++ src/main/java/ru/netology/exception/NotFoundException.java
@@ -0,0 +1,11 @@
+package ru.netology.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException() {
+
+    }
+
+    public NotFoundException(String massage) {
+
+    }
+}
